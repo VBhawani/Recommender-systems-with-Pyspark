e7d89d9166d4dd89278d178b24fd7ae0b52300c7
VB1395 <veenabhawani98@gmail.com>
2025-01-10 14:14:23 +0100

    updating app.pyfile

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 
 import streamlit as st
+import pandas as pd
 from main import load_model, movie_reco  
 
-# Load the pre-trained ALS model
+# Loading pre-trained ALS model
 model = load_model("./model")
 
 st.title("Movie Recommendation System")
@@ -11,19 +12,28 @@ st.write("""
     Enter your user ID to receive personalized movie recommendations.
 """)
 
-
+#taking input from user of userId
 user_id = st.number_input("Enter User ID", step=1, value=1)
 
 if st.button("Get Recommendations"):
     st.write(f"Fetching movie recommendations for User ID: {user_id}...")
 
-    # Get the recommended movies for the user
     recommendations = movie_reco(user_id, model)
-
-    # Check if recommendations are available
+    """it checks whether model has returned any recommendations, if yes it will take first 10 """
     if recommendations.count() > 0:
-        st.write("Here are the top 10 movie recommendations:")
-        recommendations_list = recommendations.take(10)  # Get the top 10 rows
-        st.write(recommendations_list)  
+        recommendations_list = recommendations.take(10)
+
+        """convert each row from Spark DataFrame into dictionary,"""
+        recommendations_data = []
+        for row in recommendations_list:
+            recommendations_data.append(row.asDict())
+
+        """convert dict into pandas dataframe"""
+        df_recommendations = pd.DataFrame(recommendations_data)
+
+        # Display table in Streamlit
+        st.dataframe(df_recommendations)
     else:
         st.write("No recommendations available for this user.")
+    """If there are no recommendation if the user has not rated any movies or the model could 
+    not generate recommendations, it displays the message.""""
--- app/main.py
+++ app/main.py
@@ -14,13 +14,13 @@ ratings_df = ratings_df.select(ratings_df.userId.cast("integer"), ratings_df.mov
                                ratings_df.rating.cast("double"))
 
 def create_model():
-    
+    """creating model for recommendation"""
     (train, test) = ratings_df.randomSplit([0.8,0.2], seed=42)
 
     als = ALS(regParam= 0.1,maxIter=20, userCol="userId", itemCol="movieId", ratingCol="rating", nonnegative= True,coldStartStrategy="drop")
 
     model = als.fit(train)
-    model.save(sc,"../model")
+    model.save(sc,"./model")
     prediction = model.transform(test)
     
     # Calculate Root Mean Squared Error 
@@ -35,7 +35,8 @@ def movie_reco(user_id,model):
 
     user_ratings = ratings_df.filter(ratings_df.userId == user_id)
     if user_ratings.count() == 0:
-        # If no ratings exist for the user, recommend popular movies
+        """If user has no ratings then system recommends popular movies based on highest avg 
+         ratings by users. """
         print(f"No ratings found for User {user_id}. Recommending popular movies.")
         # select top-rated movies (top 10 movies based on average ratings)
         popular_movies = ratings_df.groupBy("movieId").avg("rating").orderBy("avg(rating)", ascending=False).limit(10)
@@ -43,7 +44,7 @@ def movie_reco(user_id,model):
 
         return recommended_movies.select("title")
     else:
-        # If user has rated movies, recommend based on the model
+        """If the user has rated movies model will generates personalized recommendations"""
         recommendations = model.recommendForUserSubset(user_ratings, 10)
         recommended_movie_ids = recommendations.select(explode("recommendations.movieId").alias("movieId"))
         recommended_movies = recommended_movie_ids.join(movies_df, on="movieId", how="left_outer")
@@ -52,8 +53,8 @@ def movie_reco(user_id,model):
 
 def load_model(model_path):
     return ALSModel.load(model_path)
+
+
 #create_model()
 # movie_reco(1,model)
 
-# model = load_model("./model")
-# movie_reco(2, model)
